5b0a72aa309e4f893c1fd8a7ec51fcfbec662ea2
Mykola Kokoiev <veokoknik@gmail.com>
2024-05-12 09:51:54 +0200

    SOL

--- app/create_file.py
+++ app/create_file.py
@@ -2,7 +2,7 @@ import os
 import sys
 import datetime
 
-arg_list = [arg for arg in sys.argv]
+argument_list = [arg for arg in sys.argv]
 
 
 def create_directory(arguments: list) -> str:
@@ -27,26 +27,28 @@ def create_file(name_of_file: str) -> None:
             line_number += 1
 
 
-if "-d" in arg_list and "-f" in arg_list:
+def read_flags(arg_list: list) -> None:
+    if "-d" in arg_list and "-f" in arg_list:
 
-    if arg_list.index("-d") < arg_list.index("-f"):
-        args = arg_list[arg_list.index("-d") + 1:arg_list.index("-f")]
-        file_name = os.path.join(create_directory(args), arg_list[-1])
+        if arg_list.index("-d") < arg_list.index("-f"):
+            args = arg_list[arg_list.index("-d") + 1:arg_list.index("-f")]
+            file_name = os.path.join(create_directory(args), arg_list[-1])
 
-    else:
+        else:
+            args = arg_list[arg_list.index("-d") + 1::]
+            file_name = os.path.join(
+                create_directory(args),
+                arg_list[arg_list.index("-f") + 1]
+            )
+        create_file(file_name)
+
+    elif "-d" in arg_list:
         args = arg_list[arg_list.index("-d") + 1::]
-        file_name = os.path.join(
-            create_directory(args),
-            arg_list[arg_list.index("-f") + 1]
-        )
-    create_file(file_name)
+        create_directory(args)
+
+    elif "-f" in arg_list:
+        file_name = os.path.join(os.getcwd(), arg_list[-1])
+        create_file(file_name)
 
 
-elif "-d" in arg_list:
-    args = arg_list[arg_list.index("-d") + 1::]
-    create_directory(args)
-
-
-elif "-f" in arg_list:
-    file_name = os.path.join(os.getcwd(), arg_list[-1])
-    create_file(file_name)
+read_flags(argument_list)
